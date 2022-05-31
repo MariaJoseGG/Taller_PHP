b84d75374633a380c5416247218967ebb1a58d54
Alexandra565 <tbetancurth@unal.edu.co>
2022-05-31 10:28:24 -0500

    se sube el formulario de registro, con la validación

--- singup.php
+++ singup.php
@@ -2,21 +2,28 @@
 include("db.php");
 
 if(isset($_POST['save'])) {
-    $iden = $_POST['iden'];
-    $nom = $_POST['nom'];
-    $fechaNaci = $_POST['fechaNaci'];
-    $papa = $_POST['papa'];
+    $user = $_POST['user'];
+    $iden = password_hash($_POST['iden'],PASSWORD_BCRYPT);
 
-    $query = "INSERT INTO estudiante(id,nombre,fechaNacimiento,PAPA) VALUES($iden, '$nom', '$fechaNaci', $papa)";
-    $result = mysqli_query($conn,$query);
-    if(!$result) {
-        $_SESSION['mensaje'] = "No se pudo guardar";
-        $_SESSION['tipo_mensaje'] = "danger";
+    $query1 = "SELECT * FROM usuario WHERE nombre='$user'";
+    $result = mysqli_query($conn,$query1);
+    $vec=mysqli_fetch_array($result);
+    if($vec>0){
+        $_SESSION['mensaje_registro'] = "No se pudo guardar";
+        $_SESSION['tipo-mensaje'] = "danger";
     }
-    else {
-        $_SESSION['mensaje'] = "Estudiante guardado con éxito";
-        $_SESSION['tipo_mensaje'] = "success";
+    else{
+         $query2 = "INSERT INTO usuario(user,iden) VALUES( '$user','$iden')";
+         $result2 = mysqli_query($conn,$query2);  
+         if(!$result2) {
+            $_SESSION['mensaje_registro'] = "No se pudo guardar";
+            $_SESSION['tipo-mensaje'] = "danger";
+        }
+        else {
+            $_SESSION['mensaje_registro'] = "Usuario guardado con éxito";
+            $_SESSION['tipo-mensaje'] = "success";
+        } 
     }
-    header("Location: index.php");
+    header("Location: registro.php");
 }
 ?>
